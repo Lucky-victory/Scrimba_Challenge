cefc9c2d33a9155455842597e58c8d06805e926d
lucky-victory <blizwonder25@gmail.com>
2021-07-19 12:13:49 +0100

    updated index.html,style.css and main.js

--- Dog_Basket/main.js
+++ Dog_Basket/main.js
@@ -1,65 +1,77 @@
-// const DOG_API_KEY='f3ee6048-7fe7-457a-8068-68d52b50583d';
-// async function fetchDogs(){
-//    let fetchedData;
-//    try{
-      
-   
-//    const res= await fetch(`https://api.thedogapi.com/v1/images/search?api_key=${DOG_API_KEY}`);
-//     fetchedData= res.json();
-//    }
-//    catch(err){
-//       if(err)
-//       throw err;
-//    }
-//    return fetchedData
-// }
 
-// function renderHtml(dogs) {
-//    //let c =  dogs.then(d => d)
-//    //console.log(c);
-//    let img=document.createElement('img');
-//    img.width='400'
-//    img.src=dogs[0].url
-// console.log(dogs);
-//    document.body.appendChild(img)
-
-// }
-//  fetchDogs().then(data => renderHtml(data))
-const lightBoxImage=document.querySelector('.lightbox-image')
-const lightBoxImageWrapper=document.querySelector('.lightbox-image-wrapper')
-const likeBtns=document.querySelectorAll('.like--btn');
-const postImages=document.querySelectorAll('.post__image');
-likeBtns.forEach((likeBtn,i)=>{
-   likeBtn.addEventListener('click',triggerLike)
+const toastElem=document.querySelector('.toast');
+ const lightBoxImage = document.querySelector('.lightbox-image')
+const lightBoxImageWrapper = document.querySelector('.lightbox-image-wrapper')
+const likeBtns = document.querySelectorAll('.like--btn');
+const shareBtns = document.querySelectorAll('.share--btn');
+const postImages = document.querySelectorAll('.post__image');
+shareBtns.forEach((shareBtn, i) => {
+   shareBtn.addEventListener('click', triggerShare)
 });
-function triggerLike(e){
 
-      let message = e.target.parentElement.parentElement.querySelector('.like-message');
-   message.textContent='';
-   e.target.textContent='like';
-      e.target.classList.toggle('fi-sr-heart');
-      if(e.target.classList.contains('fi-sr-heart')){
-         e.target.textContent='liked'
-            message.textContent = 'you liked this'
-      }
+likeBtns.forEach((likeBtn, i) => {
+   likeBtn.addEventListener('click', triggerLike)
+});
+
+function triggerLike(e) {
+
+   let message = e.target.parentElement.parentElement.querySelector('.like-message');
+   message.textContent = '';
+   e.target.textContent = 'like';
+   e.target.classList.toggle('fi-sr-heart');
+   if (e.target.classList.contains('fi-sr-heart')) {
+      e.target.textContent = 'liked'
+      message.textContent = 'you liked this'
+   }
 
 }
 
 
 
 
-postImages.forEach((postImage,i)=>{
-   postImage.addEventListener('click',triggerLightbox(i))
+postImages.forEach((postImage, i) => {
+   postImage.addEventListener('click', triggerLightbox(i))
 });
-function triggerLightbox(i){
-   
-   return function(){
-   lightBoxImage.src=postImages[i].src;
-   lightBoxImageWrapper.classList.add('show')
+
+function triggerLightbox(i) {
+
+   return function() {
+      lightBoxImage.src = postImages[i].src;
+      lightBoxImageWrapper.classList.add('show')
    }
 }
 
 
-lightBoxImageWrapper.addEventListener('click',()=>{
+lightBoxImageWrapper.addEventListener('click', () => {
    lightBoxImageWrapper.classList.remove('show')
-});   
+});
+
+function triggerShare(e) {
+   const { image, text } = e.target.dataset;
+
+   if (navigator.share) {
+      navigator.share({
+         files: [image],
+         title: 'Check out these Dogs',
+         text,
+         url: 'https://lucky-victory.github.io/Scrimba_Challenge/Dog_Basket'
+      }).then(() => {
+         toastElem.textContent=' Thanks for Sharing.'
+         toastElem.classList.add('showToast');
+         setTimeout(() => {
+            toastElem.classList.remove('showToast');
+
+         },5000)
+      });
+   }
+   else{
+   toastElem.textContent='Your system does not support share API. try a newer version of Chromium browser.'
+     toastElem.classList.add('showToast');
+         setTimeout(() => {
+            toastElem.classList.remove('showToast');
+
+         },5000);
+  
+   }
+   console.log(image, text);
+}
